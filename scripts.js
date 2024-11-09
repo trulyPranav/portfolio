0a1005a09a9f53a891ece9b6f9d1e38ab72e46f9
Pranav M <pranavm265@gmail.com>
2024-11-09 12:49:59 +0530

    day 2 add 3

--- scripts.js
+++ scripts.js
@@ -4,3 +4,31 @@ const navbar = document.querySelector(".navbar");
 menuIcon.addEventListener("click", () => {
     navbar.classList.toggle("change")
 })
+
+const mainTextDisplayContents = ["Flutter", "Web", ".NET"];
+let currIndex = 0;
+
+function typeWord(word, element) {
+    let charIndex = 0;
+    element.innerHTML = '';
+    let typingInterval = setInterval(() => {
+        element.innerHTML += word[charIndex];
+        charIndex++;
+        if (charIndex === word.length) {
+            clearInterval(typingInterval);
+        }
+    }, 150);
+}
+
+function displayWords() {
+    const wordElem = document.querySelector(".main-display-text-change");
+    const currentWord = mainTextDisplayContents[currIndex];
+    typeWord(currentWord, wordElem);
+    currIndex++;
+    if (currIndex === mainTextDisplayContents.length)
+        currIndex = 0;
+    setTimeout(displayWords, currentWord.length * 150 + 2000);
+    
+}
+
+displayWords();
