5e3a2fdada32cf9d0ce9735cbae516267cc3fef7
Pranav M <pranavm265@gmail.com>
2025-03-15 15:04:57 +0530

    basic site

---
+++ components/AreaOne.jsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { useState, useEffect } from "react";
+
+export default function Area1() {
+  const greetings = [
+    "hello",    // English
+    "hola",     // Spanish
+    "bonjour",  // French
+    "ciao",     // Italian
+    "hallo",    // German
+  ];
+
+  const [currentGreeting, setCurrentGreeting] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentGreeting((prev) => (prev + 1) % greetings.length);
+    }, 4000); // Change greeting every 3 seconds
+
+    return () => clearInterval(interval); // Clean up interval on component unmount
+  }, []);
+
+  return (
+    <div className="top-28 xl:top-20 relative min-h-screen flex flex-col items-baseline ml-8 xl:ml-10">
+      <motion.p
+        className="text-[16vw] xl:text-[14vw] leading-tight xl:leading-none font-medium"
+        key={currentGreeting}
+        initial={{ opacity: 0, rotateX: -60 }}
+        animate={{ opacity: 1, rotateX: 0 }}
+        exit={{ opacity: 0, rotateX: 60 }}
+        transition={{ duration: 1 }}
+      >
+        {greetings[currentGreeting]}
+      </motion.p>
+      <p className="text-[16vw] xl:text-[14vw] leading-tight xl:leading-none font-medium">
+        I'm Pranav!
+      </p>
+    </div>
+  );
+}
---
+++ containers/MainContainer.jsx
@@ -0,0 +1,9 @@
+"use client"
+
+export default function MainContainer({title, text}){
+    return (
+        <div className="h-20 w-3/4 border-white border-4 rounded-lg px-3 py-2">
+            {title}
+        </div>
+    );
+};
